02ada1ae0547117a460a5844921602710f982d3c
samefede <federico.farinia@gmail.com>
2024-04-13 23:05:25 -0300

    todo-app

--- React/todo-app/src/App.jsx
+++ React/todo-app/src/App.jsx
@@ -1,41 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const addTodo = () => {
+    const todoName = prompt("Nombre de la tarea: ")
+    setList([...list, { name: todoName, completed: false }])
+    setCount(count + 1)
+  }
+
+  function deleteTodo(index, element) {
+    if(!element.completed) setCount(count - 1)
+    setList(list.filter((_, i) => i !== index))
+  }
+
+  const checkTodo = (index) => {
+    const updatedList = [...list];
+    updatedList[index].completed = !updatedList[index].completed;
+    if(!updatedList[index].completed) setCount(count + 1);
+    else setCount(count - 1);
+  };
 
   return (
     <>
       <div className="container center">
         <h1 className="center title">TODO App</h1>
         <div className="flow-right controls">
-          <span>Item count: <span id="item-count">0</span></span>
-          <span>Unchecked count: <span id="unchecked-count">0</span></span>
+          <span>Item count: <span id="item-count">{list.length}</span></span>
+          <span>Unchecked count: <span id="unchecked-count">{count}</span></span>
         </div>
-        <button className="button center" onClick="addTodo()">Agregar Tarea</button>
+        <button className="button center" onClick={addTodo}>Agregar Tarea</button>
         <ul id="todo-list" className="todo-list">
-          <li className="todo-container">
-            <input type="checkbox" className="todo-checkbox"/>
-            <span className="todo-item">Tarea 1</span>
-            <button className="button todo-delete" onClick="deleteTodo()">Eliminar</button>
-          </li>
-          <li className="todo-container">
-            <input type="checkbox" className="todo-checkbox"/>
-            <span className="todo-item">Tarea 1</span>
-            <button className="button todo-delete" onClick="deleteTodo()">Eliminar</button>
-          </li>
-          <li className="todo-container">
-            <input type="checkbox" className="todo-checkbox"/>
-            <span className="todo-item">Tarea 1</span>
-            <button className="button todo-delete" onClick="deleteTodo()">Eliminar</button>
-          </li>
-          <li className="todo-container">
-            <input type="checkbox" className="todo-checkbox"/>
-            <span className="todo-item">Tarea 1</span>
-            <button className="button todo-delete" onClick="deleteTodo()">Eliminar</button>
-          </li>
+          {list.map((element, index) => (
+            <li className="todo-container" key={index}>
+              <input type="checkbox" className="todo-checkbox" checked={element.completed} onChange={() => checkTodo(index)} />
+              <span className="todo-item">{element.name}</span>
+              <button className="button todo-delete" onClick={() => deleteTodo(index, element)}>Eliminar</button>
+            </li>
+          ))}
         </ul>
       </div>
     </>
